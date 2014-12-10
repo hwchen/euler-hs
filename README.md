a38b1c882e46e338c882236dde286111f8a16ac1
hwchen <walther.chen@gmail.com>
2014-12-10 00:32:16 -0500

    finished 25 problems

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Euler Project
 
 Using Haskell.
+
+Finished 25 problems! Lots of slow code, but enjoying the problem solving.
+
+I'll probably go back an optimize at a later date.
--- euler035.hs
+++ euler035.hs
@@ -3,8 +3,13 @@
 -- circular prime: all permutations of prime are also prime
 -- only 14 seconds compiled -O2. Forever in ghci.
 
+-- if I want to improve, any number that contains an even digit
+-- or 0 or 5 cannot be a circular prime.
+-- someday I'll come back and get it down to 2 seconds. Maybe
+-- I have too many conversions. For now,
+-- I love the expressiveness of the code.
+
 import Utils (isPrime, intToList)
-import Data.List (permutations)
 
 primeList :: Integral a => a -> [a]
 primeList n = filter isPrime [1..n]
