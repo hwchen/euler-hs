6c1f685f53f339cfa184080bb11974b9fd6e37e0
Walther Chen <walther.chen@gmail.com>
2015-01-16 13:08:33 -0500

    finished euler 61

--- euler061.hs
+++ euler061.hs
@@ -5,6 +5,7 @@
 
 -- generate chains, or go for all combinations?
 
+import Data.List
 import Utils (intToList)
 
 polyList :: (Int -> Int) -> [Int]
@@ -24,31 +25,71 @@ hexa = polyList hexaFn
 hept = polyList heptaFn
 oct = polyList octaFn
 
--- find xlist whose numbers start with end of x-1 list 
--- then chain
+-- Find all cyclic chains first.
+-- all chains have to pass through octa, so use that as seed!
+-- at each step of chain reduce pool? or check afterwards?
 
---refactor using Maybe
+-- works only for comparing first to to last to of 4-digit numbers
+appendToChain :: [Int] -> Int -> [Int]
+appendToChain ns n
+    | drop 2 lastOfNs' == take 2 n' = ns ++ [n]
+    | otherwise                     = []
+    where lastOfNs' = intToList (last ns) 10
+          n'        = intToList n 10
 
-chain :: Int -> Int -> [Int]
-chain x y
-    | xEnd == yFront = [x,y]
-    | otherwise = []
-    where xEnd   = drop 2 $ intToList x 10 -- specific to problem
-          yFront = take 2 $ intToList y 10
+-- initial values from oct list, and mapped to lists to start chain
+initValues :: [Int] -> [[Int]]
+initValues = map (\n -> [n])
 
--- takes first list and matches to each possible choice in second list
-buildChains :: [Int] -> [Int] -> [[Int]]
-buildChains xs ys = filter (/= init xs) $ map (\y -> init xs ++ (last xs) `chain` y) ys
+-- takes seed chain and list of possible appends, returns new chains
+chainFromSeed :: [Int] -> [Int] -> [[Int]]
+chainFromSeed ns = filter (/= []) . map (appendToChain ns)
 
--- chains to chains
--- tries all elements in first list against end of all chains in second list
-cToC :: [Int] -> [[Int]] -> [[Int]]
-cToC ys xss = concatMap (flip buildChains ys) xss
+-- generate all chains starting from octa values
+notOct = concat [tri, sq, pent, hexa, hept]
 
-initializeList :: [Int] -> [[Int]]
-initializeList xs = map (\x -> [x]) xs
+-- test to see what generating all chains from oct does
+twoChains :: [[Int]]
+twoChains = concatMap (flip chainFromSeed notOct) (initValues oct)
 
+-- generate all chains of length 6
+allChains :: [[Int]]
+allChains =  concat $ drop 5 $ take 6 $ iterate (concatMap (flip chainFromSeed notOct)) (initValues oct)
+
+-- check for cyclic chains 
+isCyclic :: [Int] -> Bool
+isCyclic xs
+    | drop 2 lastOfXs' == take 2 firstOfXs' = True
+    | otherwise                             = False
+    where lastOfXs' = intToList (last xs) 10
+          firstOfXs' = intToList (head xs) 10
+
+-- test for membership in each set of figurative
+-- any of xs is an elem of ys
+anyIsElemOf :: [Int] -> [Int] -> Bool
+anyIsElemOf xs ys = foldr (anyTrue) False $ map (`elem` ys) xs
+    where anyTrue a acc = a || acc
+
+hasAllFigurative :: [Int] -> Bool
+hasAllFigurative xs = anyIsElemOf xs tri &&
+                      anyIsElemOf xs sq && 
+                      anyIsElemOf xs pent &&
+                      anyIsElemOf xs hexa && 
+                      anyIsElemOf xs hept && 
+                      anyIsElemOf xs oct 
+
+-- after inspecting output, noticed that there can be duplicates
+-- between hexa and tri lists
+triAndHexaNotSame :: [Int] -> Bool
+triAndHexaNotSame xs = filter (`elem` tri) xs /= filter (`elem` hexa) xs
+
+testList :: [Int]
+testList = [9633,3367,6724,2415,1540,4096] 
+
+-- interesting, it actually runs faster when sum than when just printing 
+-- out the lists
 main = do
-    let initial = initializeList $ polyList triangleFn
-        allChain = cToC oct $ cToC hept $ cToC hexa $ cToC pent $ cToC sq initial
-    print allChain 
+    print $ sum $ head $
+            filter triAndHexaNotSame $ 
+            filter hasAllFigurative $ 
+            filter isCyclic allChains 
